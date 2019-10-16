a822d9e40be92a4fda3e093c672811dab8a5ff4f
Laura Devos <laura.devos01@student.ap.be>
2019-10-16 11:54:48 +0200

    Database

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request');
 const app = express();
 
 const path = require('path');
@@ -36,3 +37,22 @@ app.get('/filter', function (req, res) {
 
 
 app.listen(port);
+
+request('https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_publiek3/MapServer/202/query?where=1%3D1&outFields=*&outSR=4326&f=json',
+  function(error, response, body){
+    var data = JSON.parse(body);
+
+    for(var i=0; i < data.features.length; i++) {
+        console.log("naam: " + data.features[i].attributes.soort);
+        console.log("naam: " + data.features[i].attributes.locatie);
+        console.log("naam: " + data.features[i].attributes.marktdag1);
+        console.log("naam: " + data.features[i].attributes.marktdag2);
+        console.log("naam: " + data.features[i].attributes.van);
+        console.log("naam: " + data.features[i].attributes.tot);
+        console.log("naam: " + data.features[i].attributes.district);
+          
+        console.log("coord: " + data.features[i].geometry.x + ", " + data.features[i].geometry.y);
+        console.log("");
+    }
+}
+);
